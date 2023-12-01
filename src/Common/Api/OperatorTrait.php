1399b7532ecddcbe7e2f9330ac8e1b8d57a72c7b
nortoh <christian.horton@nexcess.net>
2023-12-01 16:32:34 -0600

    Fixed uri_template

--- src/Common/Api/OperatorTrait.php
+++ src/Common/Api/OperatorTrait.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace OpenStack\Common\Api;
 
 use GuzzleHttp\Promise\PromiseInterface;
-use function GuzzleHttp\uri_template;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\UriTemplate\UriTemplate;
 use OpenStack\Common\Resource\ResourceInterface;
 use OpenStack\Common\Transport\RequestSerializer;
 use Psr\Http\Message\ResponseInterface;
@@ -113,7 +113,7 @@ trait OperatorTrait
 
         $options = (new RequestSerializer())->serializeOptions($operation, $userValues);
         $method  = $async ? 'requestAsync' : 'request';
-        $uri     = uri_template($operation->getPath(), $userValues);
+        $uri     = UriTemplate::expand($operation->getPath(), $userValues);
 
         return $this->client->$method($operation->getMethod(), $uri, $options);
     }
